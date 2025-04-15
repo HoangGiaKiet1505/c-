3634e04448d3bdb313c8a88fc6a8b7a0485ac13b
HoangGiaKiet1505 <24110265@student.hcmute.edu.vn>
2025-04-15 21:58:17 +0700

    update latest

--- btap.cpp
+++ btap.cpp
@@ -2,61 +2,66 @@
 using namespace std;
 
 
+
+//
 //void nhapSo (int &so) {
 //	do {
-//    	cout << "nhap 1 so de chon hinh can tinh: ";
+//    	
 //		cin >> so;
 //		if(so <= 0 || so > 4) 
 //			cout << "moi ban nhap lai so dung" << endl;
 //	} while (so <= 0 || so > 4);
 //		   
 //}
-//
-//
+////
+////
 //void hinhTamgiac (float &canhDay, float &chieuCao) {
 //	do {
-//		cout << "nhap so lieu hinh tam giac: ";
+//		cout << "nhap so canh day va chieu cao tam giac: ";
 //		cin >> canhDay >> chieuCao;
 //		if(canhDay <= 0 || chieuCao <= 0)
 //			cout << "moi ban nhap lai" << endl;
 //	} while (canhDay <= 0 || chieuCao <= 0);
 //}
-//
-//
+////
+////
 //void hinhChunhat (float &chieuDai, float &chieuRong) {
 //	do {
-//		cout << "nhap so lieu hcn: ";
+//		cout << "nhap chieu dai va chieu rong hcn: ";
 //		cin >> chieuDai >> chieuRong;
 //		if(chieuDai <= 0 || chieuRong <= 0)
 //			cout << "moi ban nhap lai" << endl;
 //	} while (chieuDai <= 0 || chieuRong <= 0);
 //}
-//
-//
+////
+////
 //void hinhTron (float &banKinh) {
 //	do {
-//		cout << "nhap so lieu hinh tron: ";
+//		cout << "nhap ban kinh hinh tron: ";
 //		cin >> banKinh;
 //		if(banKinh <= 0)
 //			cout << "moi ban nhap lai" << endl;
 // 	} while (banKinh <= 0);
 //}
-//
-//
-//
+////
+////
+////
 //void soBatky (int so, float &canhDay, float &chieuCao, float &chieuDai, float &chieuRong, float &banKinh) {
 //	switch (so) {
 //		case 1:	
+//			cout << " ban chon tich dien tich hinh tam giac" << endl;
 //			hinhTamgiac(canhDay, chieuCao);
 //			cout << "dtich hinh tam giac = canh day * chieu cao / 2 = ";
 //			cout << fixed << setprecision(2) << (canhDay * chieuCao / 2);
 //			break;
 //		case 2:
+//			cout << "ban chon tinh dien tich hinh chu nhat" << endl;
 //			hinhChunhat(chieuDai, chieuRong);
 //			cout << "dtich hcn = chieu dai * chieu rong = ";
 //			cout << fixed << setprecision(2) << (chieuDai * chieuRong);
 //			break;
 //		case 3:
+//			cout << "ban chon tinh dien tich hinh tron" << endl;
 //			hinhTron(banKinh);
 //			cout << "dtich hinh tron = pi * (ban kinh ^ 2) = ";
 //			cout << fixed << setprecision(2) << (3.14 * pow(banKinh, 2));
@@ -67,12 +72,14 @@ using namespace std;
 //			
 //	}
 //}
-//
-//
+////
+////
 //int main () {
 //	int so;
 //	float canhDay, chieuCao, chieuDai, chieuRong, banKinh;
 //	
+//	cout << "Chuong trinh tinh dien tich cac hinh co ban : \n 1. Hinh tam giac \n 2. Hinh chu nhat \n 3. Hinh tron \n 4. Thoat \n Ban hay bam 1 so (1,2,3) de chon hinh can tinh hoac bam so 4 de ket thuc :";
+//
 //	nhapSo(so);
 //	soBatky(so, canhDay, chieuCao, chieuDai, chieuRong, banKinh);	
 //	
@@ -81,7 +88,7 @@ using namespace std;
 //	
 
 
-int main () {
+//int main () {
 
 //Bai 1
 //	
@@ -119,5 +126,24 @@ int main () {
 	
 	
 	
-	return 0;
-}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- buoi4.cpp
+++ buoi4.cpp
@@ -1,6 +1,138 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+//void nhapSonguyen(int &n) {
+//	do{
+//		cout << "moi ban nhap so nguyen duong n: ";
+//		cin >> n;
+//		if(n <= 0) cout << "moi ban nhap lai";
+//} while (n <= 0);
+//}
+
+
+//int tinhS1(int n) {
+//	
+//	int S1 = 0;
+//	for(int i = 0; i <= n; i++) {
+//		
+//		if(i % 2 == 0) S1 += i;
+//		
+//	}
+//return S1;
+//}
+//
+//
+//
+//int tinhS2(int n) {
+//	int S2 = 1;
+//	for(int i = 1; i <= n; i++) {
+//		S2 *= i;
+//	}
+//return S2;
+//}
+//
+//
+//int tinhS3(int n) {
+//	int S3 = 0;
+//	for (int i = 1; i <= n; i++) {
+//			S3 += tinhS2(i);
+//}
+//return S3;
+//}
+//
+//
+//int tinhS4(int n) {
+//	int S4 = 0;
+//	for(int i = 1; i <= n; i++) {
+//		if(i % 2 != 0) S4 += pow(i, 2);
+//	}
+//return S4;
+//}
+//
+//void ketQua() {
+//	int n;
+//	nhapSonguyen(n);
+//	cout << "S1 = " << " " << tinhS1(n) << endl;
+//	cout << "S2 = " << " " << tinhS2(n) << endl;
+//	cout << "S3 = " << " " << tinhS3(n) << endl;
+//	cout << "S4 = " << " " << tinhS4(n) << endl;
+//}
+
+void nhapSonguyen(int &n) {
+	do{
+		cout << "moi ban nhap so nguyen duong n: ";
+		cin >> n;
+		if(n <= 0) cout << "moi ban nhap lai";
+	} while (n <= 0);
+}
+
+bool soNguyento(int n) {
+	if(n < 2) return false;
+	for(int i = 2; i <= sqrt(n); i++) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+
+bool soChinhphuong(int n) {
+	if(n < 3) return false;
+	int sqr = sqrt(n);
+	if(pow(sqr, 2) == n) {	
+		return true;
+	}else
+		return false;
+}
+
+
+void soLonnhat(int n) {
+	int soDu = n % 10;
+	int max = soDu;
+	int min = soDu;
+	while(n > 0){ 
+		soDu = n % 10;
+		if(soDu > max) max = soDu;
+		if(soDu < min) min = soDu;
+		n /= 10;
+	}
+	cout << "chu so lon nhat la:" << " " << max << endl;
+	cout << "chu so be nhat la: " <<  min <<endl;
+}
+
+
+
+int soDaonguoc(int n) {
+	int soDao = 0;
+	while(n > 0) {
+		soDao = soDao * 10 + (n % 10);
+		n /= 10;
+	}
+	return soDao;
+}
+
+
+void ketQua() {
+	int n;
+	nhapSonguyen(n);
+	if(soNguyento(n)) {
+	 		cout << n << " " << "la so nguyen to" << endl;
+	}else
+	 		cout << n << " " << "khong la so nguyen to" << endl;
+	 		
+	 		
+	if(soChinhphuong(n)) {
+			cout << n << " " << "la so chinh phuong" << endl;
+	}else 
+			cout << n << " " << "ko la so chinh phuong" << endl;
+			
+	soLonnhat(n);
+	cout << "so dao nguwoc la: " << soDaonguoc(n);
+}
+
 int main () {
+		
+	ketQua();
 	
-	return 0;
+return 0;
 }
