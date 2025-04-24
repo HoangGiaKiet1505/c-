ac2fe0a2abe0a6d6258040f19f2ffd898220b0e7
HoangGiaKiet1505 <24110265@student.hcmute.edu.vn>
2025-04-24 18:58:34 +0700

    latest update

--- array.cpp
+++ array.cpp
@@ -2,56 +2,110 @@
 using namespace std;
 
 
+//void nhapMang(int a[], int &n) {
+//	cout << "nhap so phan tu n: ";
+//	cin >> n;
+//	for(int i = 0; i < n; i++) {
+//		cout << "a[" << i << "] = ";
+//		cin >> a[i];
+//	}
+//}
+//
+//void xuatMang(int a[], int n) {
+//
+//	cout << "day so co " << n << " phan tu: ";
+//		for(int i = 0; i < n; i++) {
+//		cout << a[i] << " ";
+//	}
+//}
+//
+//void soDuongchiahetcho3(int a[], int n) {
+//	
+//	cout << "day so duong chia het cho 3 la: ";
+//	for(int i = 0; i < n; i++) {
+//		if((a[i] % 3 == 0) && a[i] > 0) cout<< a[i] << " "; 
+//	}
+//}
+//
+//void mangNgaunhien(int a[], int &n) {
+//	srand(time(0));
+//	cout << "nhap so phan tu n: ";
+//	cin >> n;
+//	for(int i =0; i < n; i++) {
+//	
+//		a[i] = rand()%100;
+//	}
+//	cout << "mang ngau nhien ";
+//	xuatMang(a, n);
+//}
+//
+//
+//int main () {
+//	int n;
+//	int a[100];
+//	
+//	nhapMang(a, n);
+//	xuatMang(a, n);
+//	cout << endl;
+//	soDuongchiahetcho3(a, n);
+//	cout << endl;
+//	mangNgaunhien(a, n);
+//	
+//	return 0;
+//}
+
+
+
+
 void nhapMang(int a[], int &n) {
-	cout << "nhap so phan tu n: ";
-	cin >> n;
-	for(int i = 0; i < n; i++) {
-		cout << "a[" << i << "] = ";
-		cin >> a[i];
-	}
+    cout << "Nhap so phan tu n: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+    }
 }
 
-void xuatMang(int a[], int n) {
 
-	cout << "day so co " << n << " phan tu: ";
-		for(int i = 0; i < n; i++) {
-		cout << a[i] << " ";
-	}
+void timMax(int a[], int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
+     cout << "Gia tri lon nhat trong mang: " << max << endl;
 }
 
-void soDuongchiahetcho3(int a[], int n) {
+
+void timMin(int a[], int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] < min)
+            min = a[i];
+     cout << "Gia tri nho nhat trong mang: " << min << endl;
+}
+
+void timViTriX(int a[], int n, int x) {
+	cout << "Nhap gia tri x can tim: ";
+    cin >> x;
+    bool found = false;
+    for (int i = 0; i < n; i++)
+        if (a[i] == x) {
+            cout << "x xuat hien tai vi tri: " << i << endl;
+            found = true;
+   		} 
+   		if(found != true) cout << "Khong tim thay x trong mang" << endl;
+}
+
+
+int main() {
+    int a[100], n, x;
+    
+    nhapMang(a, n);
 	
-	cout << "day so duong chia het cho 3 la: ";
-	for(int i = 0; i < n; i++) {
-		if((a[i] % 3 == 0) && a[i] > 0) cout<< a[i] << " "; 
-	}
+	timViTriX(a, n, x);
+	
+    return 0;
 }
 
-void mangNgaunhien(int a[], int &n) {
-	srand(time(0));
-	cout << "nhap so phan tu n: ";
-	cin >> n;
-	for(int i =0; i < n; i++) {
-	
-		a[i] = rand()%100;
-	}
-	cout << "mang ngau nhien ";
-	xuatMang(a, n);
-}
-
-
-int main () {
-	int n;
-	int a[100];
-	
-	nhapMang(a, n);
-	xuatMang(a, n);
-	cout << endl;
-	soDuongchiahetcho3(a, n);
-	cout << endl;
-	mangNgaunhien(a, n);
-	
-	return 0;
-}
 
 
---
+++ btap_chuong23.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+//void nhapSonguyen(int &n) {
+//	do{
+//		cout << "moi ban nhap so nguyen duong n: ";
+//		cin >> n;
+//		if(n <= 0 || n >= 10) cout << "moi ban nhap lai" << endl;
+//} while (n <= 0 || n >= 10);
+//}
+//
+//int main() {
+//	int n;	
+//	nhapSonguyen(n);
+//    
+//	cout << "Cac so co 2 chu so sao cho tong 2 chu so bang " << n << " la: " << endl;
+//
+//    for (int i = 10; i < 100; i++) {
+//        int hangChuc = i / 10;
+//        int donVi = i % 10;
+//        if (hangChuc + donVi == n) {
+//            cout << i << " ";
+//        }
+//    }
+//    cout << endl;
+//    
+//    
+//    cout << "cac so co 2 chu so bang 2 lan tong cua 2 chu so do " << n << "la: " << endl;
+//    
+//    
+//    for (int i = 10; i < 100; i++) {
+//        int hangChuc = i / 10; 
+//        int donVi = i % 10;  
+//        if (hangChuc * donVi == 2 * (hangChuc + donVi)) {
+//            cout << i << " ";
+//        }
+//    }
+//
+//    return 0;
+//}
+//void nhapSonguyen(int &n) {
+//	do{
+//		cout << "moi ban nhap so nguyen duong n: ";
+//		cin >> n;
+//		if(n <= 0) cout << "moi ban nhap lai" << endl;
+//} while (n <= 0);
+//}
+//
+//void nhapHaisonguyen(int &a, int &b) {
+//	do{
+//		cout << "moi ban nhap 2 so nguyen duong a va b: ";
+//		cin >> a >> b;
+//		if(a <= 0 || b <=0) cout << "moi ban nhap lai" << endl;
+//} while (a <= 0 || b <=0);
+//}
+//
+//void inUocSo(int n) {
+//    cout << "Cac uoc so cua " << n << " la: ";
+//    for (int i = 1; i <= n; i++) {
+//        if (n % i == 0) {
+//            cout << i << " ";
+//        }
+//    }
+//    cout << endl;
+//}
+//
+//
+//int timUSCLN(int a, int b) {
+//    while (a != b) {
+//        if (a > b) {
+//            a = a - b;
+//        } else {
+//            b = b - a;
+//        }
+//    }
+//    return a; 
+//}
+//int main() {
+//	//a
+//	int n;
+//	nhapSonguyen(n);
+//	inUocSo(n);
+//	//b
+//	int a, b;
+//    nhapHaisonguyen(a, b);
+//    cout << "Uoc so chung lon nhat cua " << a << " va " << b << " la: " << timUSCLN(a, b) << endl;
+//
+//    return 0;
+//}
+
+
+//void inDayfibonacci(int n) {
+//    int a = 0, b = 1, c;
+//    cout << "15 so dau tien cua day Fibonacci: ";
+//    for (int i = 0; i < n; ++i) {
+//        cout << a << " ";
+//        c = a + b;
+//        a = b;
+//        b = c;
+//    }
+//    cout << endl;
+//}
+//
+//int main() {
+//    inDayfibonacci(15);
+//    return 0;
+//}
+
+
+
+
