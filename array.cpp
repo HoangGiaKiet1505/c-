414f1231232750e89251f6edbc515f913272b530
HoangGiaKiet1505 <24110265@student.hcmute.edu.vn>
2025-05-06 19:54:03 +0700

    update

--- array.cpp
+++ array.cpp
@@ -57,55 +57,133 @@ using namespace std;
 
 
 
-void nhapMang(int a[], int &n) {
-    cout << "Nhap so phan tu n: ";
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
-    }
-}
+//void nhapMang(int a[], int &n) {
+//    cout << "Nhap so phan tu n: ";
+//    cin >> n;
+//    for (int i = 0; i < n; i++) {
+//        cout << "a[" << i << "] = ";
+//        cin >> a[i];
+//    }
+//}
+//
+//
+//void timMax(int a[], int n) {
+//    int max = a[0];
+//    for (int i = 1; i < n; i++)
+//        if (a[i] > max)
+//            max = a[i];
+//     cout << "Gia tri lon nhat trong mang: " << max << endl;
+//}
+//
+//
+//void timMin(int a[], int n) {
+//    int min = a[0];
+//    for (int i = 1; i < n; i++)
+//        if (a[i] < min)
+//            min = a[i];
+//     cout << "Gia tri nho nhat trong mang: " << min << endl;
+//}
+//
+//void timViTriX(int a[], int n, int x) {
+//	cout << "Nhap gia tri x can tim: ";
+//    cin >> x;
+//    bool found = false;
+//    for (int i = 0; i < n; i++)
+//        if (a[i] == x) {
+//            cout << "x xuat hien tai vi tri: " << i << endl;
+//            found = true;
+//   		} 
+//   		if(found != true) cout << "Khong tim thay x trong mang" << endl;
+//}
+//
+//
+//int main() {
+//    int a[100], n, x;
+//    
+//    nhapMang(a, n);
+//	
+//	timViTriX(a, n, x);
+//	
+//    return 0;
+//}
 
 
-void timMax(int a[], int n) {
-    int max = a[0];
-    for (int i = 1; i < n; i++)
-        if (a[i] > max)
-            max = a[i];
-     cout << "Gia tri lon nhat trong mang: " << max << endl;
-}
 
-
-void timMin(int a[], int n) {
-    int min = a[0];
-    for (int i = 1; i < n; i++)
-        if (a[i] < min)
-            min = a[i];
-     cout << "Gia tri nho nhat trong mang: " << min << endl;
-}
-
-void timViTriX(int a[], int n, int x) {
-	cout << "Nhap gia tri x can tim: ";
-    cin >> x;
-    bool found = false;
-    for (int i = 0; i < n; i++)
-        if (a[i] == x) {
-            cout << "x xuat hien tai vi tri: " << i << endl;
-            found = true;
-   		} 
-   		if(found != true) cout << "Khong tim thay x trong mang" << endl;
-}
-
-
-int main() {
-    int a[100], n, x;
-    
-    nhapMang(a, n);
-	
-	timViTriX(a, n, x);
-	
-    return 0;
-}
+//void boSungCuoi(int A[], int &n, int x) {
+//    A[n] = x;
+//    n++;
+//}
+//
+//
+//void xoaTaiViTri(int A[], int &n, int k) {
+//    if (k < 0 || k >= n) {
+//        cout << "Vi tri khong hop le" << endl;
+//        return;
+//    }
+//    for (int i = k; i < n - 1; i++) {
+//        A[i] = A[i + 1];
+//    }
+//    n--;
+//}
+//
+//
+//void chenTaiViTri(int A[], int &n, int x, int k) {
+//    if (k < 0 || k > n) {
+//        cout << "Vi tri khong hop le" << endl;
+//        return;
+//    }
+//    for (int i = n; i > k; i--) {
+//        A[i] = A[i - 1];
+//    }
+//    A[k] = x;
+//    n++;
+//}
+//
+//
+//void inMang(int A[], int n) {
+//    cout << "Mang hien tai: ";
+//    for (int i = 0; i < n; i++) {
+//        cout << A[i] << " ";
+//    }
+//    cout << endl;
+//}
+//
+//int main() {
+//    int A[1000]; 
+//    int n, x, k;
+//    
+//    cout << "Nhap so phan tu ban dau: ";
+//    cin >> n;
+//    cout << "Nhap cac phan tu:\n";
+//    for (int i = 0; i < n; i++) {
+//        cin >> A[i];
+//    }
+//
+//    inMang(A, n);
+//
+//
+//    cout << "Nhap x can bo sung cuoi mang: ";
+//    cin >> x;
+//    boSungCuoi(A, n, x);
+//    inMang(A, n);
+//
+// 
+//    cout << "Nhap vi tri k can xoa: ";
+//    cin >> k;
+//    xoaTaiViTri(A, n, k);
+//    inMang(A, n);
+//
+// 
+//    cout << "Nhap gia tri x can chen: ";
+//    cin >> x;
+//    cout << "Nhap vi tri k can chen vao: ";
+//    cin >> k;
+//    chenTaiViTri(A, n, x, k);
+//    inMang(A, n);
+//
+//    return 0;
+//}
+//
 
 
 
